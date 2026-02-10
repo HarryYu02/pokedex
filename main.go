d5486660ce05b701e76e4b89234fcb359ee7f8a4
Harry Y. <harryyu02@gmail.com>
2026-02-10 13:38:33 -0500

    add simple repl

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
+	"os"
+	"bufio"
 	"fmt"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println("Welcome to Pokedex cli!")
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		ok := scanner.Scan()
+		if !ok {
+			return
+		}
+		input := scanner.Text()
+		cleaned := cleanInput(input)
+		fmt.Printf("Your command was: %s\n", cleaned[0])
+	}
 }
